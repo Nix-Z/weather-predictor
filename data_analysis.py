45741ffb33e1cd7df39a89333c78733d61c12e32
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-16 15:27:33 -0800

    Data Analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,19 +1,27 @@
-import pandas as pd
 from data_extraction import load_data
 
 def analyze_data():
   data = load_data()
   print(data.describe())
   print(data.info())
-  print(f"Data Features: {data.columns}") # Lists data features
+
+  print(f"\nData Features: {data.columns}") # Lists data features
+  
+  print(f"\nEmpty Values: ")
   print(data.isnull().sum()) # Number of null values per feature
-  print(data.duplicated().sum()) # Number of duplicated values per feature
+
+  print("\nDuplicated Values: ")
+  for column in data.columns:
+    print(column, data[column].duplicated().sum()) # Number of duplicated values per feature
+
+  print("\nUnique Values: ")
   for column in data.columns:
     print(column, data[column].nunique()) # Number of unique values per feature
+
   categorical_features = data.select_dtypes("object").columns
-  print(f"Categorical Features: {categorical_features}")
+  print(f"\nCategorical Features: {categorical_features}")
   numerical_features = data.select_dtypes("number").columns
-  print(f"Numerical Features: {numerical_features}")
+  print(f"\nNumerical Features: {numerical_features}")
   return data, categorical_features, numerical_features
   
 analyze_data()
