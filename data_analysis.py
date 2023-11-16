78ba56678b66a5f9c072dbf990160f29e031814b
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-16 15:09:52 -0800

    Data Analysis

--- data_analysis.py
+++ data_analysis.py
@@ -6,22 +6,14 @@ def analyze_data():
   print(data.describe())
   print(data.info())
   print(f"Data Features: {data.columns}") # Lists data features
-  check_values = []
+  print(data.isnull().sum()) # Number of null values per feature
+  print(data.duplicated().sum()) # Number of duplicated values per feature
   for column in data.columns:
-    #print(column, data[column].isnull().sum()) # Number of null values per feature
-    #print(column, data[column].duplicated().sum())  # Number of duplicated values per feature
-    #print(column, data[column].nunique()) # Number of unique values per feature
-
-    data_types = data[column].dtypes # dtype of feature
-    sum_null = data[column].isnull().sum()
-    sum_dup = data[column].duplicated().sum()
-    unique_values = data[column].nunique()
-    check_values.append([column, data_types, sum_null, sum_dup, unique_values,])
-  check_values = pd.DataFrame(1)
-  check_values.columns = ['Feature', 'Data Type', '# of Null Values', '# of Duplicate Values', '# of Unique Values']
-  print(check_values)
+    print(column, data[column].nunique()) # Number of unique values per feature
   categorical_features = data.select_dtypes("object").columns
+  print(f"Categorical Features: {categorical_features}")
   numerical_features = data.select_dtypes("number").columns
+  print(f"Numerical Features: {numerical_features}")
   return data, categorical_features, numerical_features
   
 analyze_data()
