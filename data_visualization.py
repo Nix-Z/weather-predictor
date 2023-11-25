b6322de36b0d6745f45e9e95ec6278a5169d126a
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-25 14:32:53 -0800

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -4,4 +4,13 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from data_preprocess import process_data
+import plotly.figure_factory as ff
+from data_preprocess import preprocess_data
+
+fig_array = []
+def visualize_data():
+  data, categorical_features, numerical_features = preprocess_data()
+
+  for numerical_feature in numerical_features:
+
+  for categorical_feature in categorical_features:
