0f918f1186491470746e759ce22ffb59da548fda
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-25 19:30:33 -0800

    Add files via upload

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,8 +1,8 @@
-from data_extraction import analyze_data
+from data_analysis import analyze_data
 
 def preprocess_data():
   data, categorical_features, numerical_features = analyze_data()
-  data.drop['Date', 'WindGustDir', 'WindDir9am', 'WindDir3am']
+  data.drop(['Date', 'WindGustDir', 'WindDir9am', 'WindDir3pm'], axis=1, inplace=True)
   print(data.head())
   categorical_features = data.select_dtypes("object").columns
   numerical_features = data.select_dtypes("number").columns
