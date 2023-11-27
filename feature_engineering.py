f8c60c95eaa2e5a13e4fcab804c9a2d342456c4f
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-27 03:32:11 -0800

    Add files via upload

---
+++ feature_engineering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from data_visualization import visualize_data
+
+def engineer_features():
+    data = visualize_data()
+
+    # Balance the Data
+    new_data = data.drop(columns=['RainToday'])
+    scaler = StandardScaler()
+    scaler.fit(new_data)
+    scaled_features = scaler.transform(new_data)
+    scaled_data = pd.DataFrame(scaled_features,columns=new_data.columns[:])
+    selected_columns = data[['RainToday']]
+    scaled_data[['RainToday']] = selected_columns.copy()
+
+    # Convert categorical feature values to binary values
+    scaled_data.replace({'RainToday': {'No':0, 'Yes':1}}, inplace=True)
+    print(scaled_data.head())
+
+    return scaled_data
+
+engineer_features()
