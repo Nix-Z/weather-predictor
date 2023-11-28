670dba02b30e75bc296c559a10f55b4907223db2
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2023-11-28 11:06:27 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -19,7 +19,7 @@ def engineer_features():
     scaled_data.replace({'RainToday': {'No':0, 'Yes':1}}, inplace=True)
     print(scaled_data.head())
 
-    data_balanced.to_csv('weather_predictor_cleansed_data.csv', index=False)
+    scaled_data.to_csv('weather_predictor_cleansed_data.csv', index=False)
 
     return scaled_data
 
